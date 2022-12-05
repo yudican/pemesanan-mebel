7dc4207604350462ff0f06676c8a19ff81c55854
Yudi Candra <yudhichandra02@gmail.com>
2022-12-05 15:52:13 +0700

    update panduan

--- resources/views/livewire/client/panduan-admin.blade.php
+++ resources/views/livewire/client/panduan-admin.blade.php
@@ -1,57 +1,22 @@
 <div class="page-inner">
     <div class="row">
         <div class="col-md-12">
-            <h3 class="text-center">
-                PANDUAN ADMIN
-            </h3>
-            <div>
-                <h3 class="text-muted">
-                    1. Data Kategori
-                </h3>
-                <img alt="Bootstrap Image Preview" src="{{asset('assets/img/panduan-admin/data-kategori.png')}}" />
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title text-capitalize">
+                        <a href="{{route('dashboard')}}">
+                            <span><i class="fas fa-arrow-left mr-3"></i>Panduan</span>
+                        </a>
+
+                    </h4>
+                </div>
             </div>
-            <div>
-                <h3 class="text-muted">
-                    2. Metode Pembayaran
-                </h3>
-                <img alt="Bootstrap Image Preview"
-                    src="{{asset('assets/img/panduan-admin/data-metode-pembayaran.png')}}" />
-            </div>
-            <div>
-                <h3 class="text-muted">
-                    3. Data Banner
-                </h3>
-                <img alt="Bootstrap Image Preview" src="{{asset('assets/img/panduan-admin/data-banner.png')}}" />
-            </div>
-            <div>
-                <h3 class="text-muted">
-                    4. Data Ongkir
-                </h3>
-                <img alt="Bootstrap Image Preview" src="{{asset('assets/img/panduan-admin/data-ongkir.png')}}" />
-            </div>
-            <div>
-                <h3 class="text-muted">
-                    5. Data Produk
-                </h3>
-                <img alt="Bootstrap Image Preview" src="{{asset('assets/img/panduan-admin/data-produk.png')}}" />
-            </div>
-            <div>
-                <h3 class="text-muted">
-                    6. Form Data Produk
-                </h3>
-                <img alt="Bootstrap Image Preview" src="{{asset('assets/img/panduan-admin/form-produk.png')}}" />
-            </div>
-            <div>
-                <h3 class="text-muted">
-                    7. Data Pesanan
-                </h3>
-                <img alt="Bootstrap Image Preview" src="{{asset('assets/img/panduan-admin/data-pesanan.png')}}" />
-            </div>
-            <div>
-                <h3 class="text-muted">
-                    8. Update Status Pesanan
-                </h3>
-                <img alt="Bootstrap Image Preview" src="{{asset('assets/img/panduan-admin/update-pesanan.png')}}" />
+        </div>
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <embed type="application/pdf" src="{{asset('assets/panduan-mebel.pdf')}}" style="width:100%;height:1000px;"></embed>
+                </div>
             </div>
         </div>
     </div>
